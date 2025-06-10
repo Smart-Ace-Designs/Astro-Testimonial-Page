2690bbbf9e87811ce4db76087707ed4e7a07db3e
Smart Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2025-06-10 14:21:24 -0500

    Move data to separate file

---
+++ src/data/data.ts
@@ -0,0 +1,20 @@
+export const reviews = [
+  {
+    name: "Samantha Lee",
+    rating: 4,
+    review: "The checklist ensures that the review process is thorough",
+    socialMedia: "/google-testimonial.svg",
+  },
+  {
+    name: "Rachel Patel",
+    rating: 5,
+    review: "I highly recommend the Writecream Business Description",
+    socialMedia: "/meta-testimonial.svg",
+  },
+];
+
+export const features = [
+  "Checklist to Review an Academic Paper",
+  "Peer Review Checklist",
+  "Checklist for Editors, Reviewers, and Authors of SPIE Journals",
+];
